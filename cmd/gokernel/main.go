1d98c8518301472d60a96dd914a0c4a63d57f2a1
Jack Clayton <jackos@me.com>
2021-08-12 16:44:36 +0800

    Rewriting kernel

---
+++ cmd/gokernel/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"regexp"
+
+	"golang.org/x/tools/go/packages"
+)
+
+type Page struct {
+	File string
+	Body []byte
+}
+
+func (p *Page) save() error {
+	var buf bytes.Buffer
+	pkgs, err := packages.Load(nil, "std")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Fprintf(&buf, "package main\n\nimport (	\n")
+	for _, v := range pkgs {
+		matches, _ := regexp.Match(v.Name, p.Body)
+		if matches {
+			fmt.Fprintf(&buf, "	\"%v\"\n", v.Name)
+		}
+	}
+	fmt.Fprintf(&buf, ")\n\nfunc main() {\n%s\n}", string(p.Body))
+	return os.WriteFile(p.File, buf.Bytes(), 0600)
+}
+
+func run(filename string) (*Page, error) {
+	out, err := exec.Command("go", "run", filepath.Join(filename)).Output()
+	if err != nil {
+		fmt.Fprint(os.Stderr, err)
+	}
+	return &Page{File: filename, Body: out}, nil
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	b := make([]byte, r.ContentLength)
+	_, err := r.Body.Read(b)
+	if err == io.EOF {
+		log.Println("Parsed message")
+	} else if err != nil {
+		log.Println("Error reading request body", err)
+	}
+	p1 := &Page{File: "subProgram/main.go", Body: b}
+	err = p1.save()
+	if err != nil {
+		log.Println("Failed to save program:", err)
+	}
+	result, err := run("subProgram/main.go")
+	if err != nil {
+		log.Println("Could not execute program:", err)
+	}
+	_, err = w.Write(result.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
---
+++ cmd/gokernel/subProgram/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	fmt.Println("My god this is cool")
+	log.Fatal("wow log as well")
+	fmt.Print("I now have an interpreter")
+}
